bc7e6833c5c121ab8096a5480f447d7b8146cad1
aneeshgarg1998 <aneeshgarg1998@gmail.com>
2021-05-29 11:47:01 +0530

    git ignore

--- src/main/java/com/hackon/trap/core/MySQLHealthCheckResource.java
+++ src/main/java/com/hackon/trap/core/MySQLHealthCheckResource.java
@@ -1,25 +1,10 @@
 package com.hackon.trap.core;
 
 import com.codahale.metrics.health.HealthCheck;
-import com.hackon.trap.core.services.EmployeeService;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
 
 public class MySQLHealthCheckResource extends HealthCheck {
-
-//    private static final String HEALTHY_MESSAGE = "The Dropwizard blog Service is healthy for read and write";
-//    private static final String UNHEALTHY_MESSAGE = "The Dropwizard blog Service is not healthy. ";
-//
-//    private final EmployeeService employeeService;
-//
-//    public MySQLHealthCheckResource(EmployeeService employeeService) {
-//        this.employeeService = employeeService;
-//    }
-//
-//    @Override
-//    protected Result check() throws Exception {
-//
-//    }
 
     private final DBI dbi;
 
